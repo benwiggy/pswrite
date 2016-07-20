5a8a95c2a0c4d5895c0a5351fe753c471882665a
John Daniel <jwdaniel@uw.edu>
2016-07-20 08:35:14 -0700

    Installer doesn't work after cloning from GitHub. Add logic to setup and teardown staging areas. Tweak permissions in staging area.

--- installers/build.sh
+++ installers/build.sh
@@ -1,1 +1,43 @@
+#!/bin/sh
+
+# Build the staging area.
+mkdir -p root/Library/Printers/PPDs/Bens
+mkdir -p root/usr/libexec/cups/backend
+
+cp ../PPDs/Direct_PDF.ppd root/Library/Printers/PPDs/Bens
+cp ../PPDs/PostScript.ppd root/Library/Printers/PPDs/Bens
+
+cp ../backend/pswrite root/usr/libexec/cups/backend
+cp ../backend/pdfwrite root/usr/libexec/cups/backend
+
+# Add the postflight script.
+mkdir -p scripts
+
+cp "../Create Print queues.command" scripts/postinstall
+
+# Fixup permissions.
+chmod ugo+x root/usr/libexec/cups/backend/pdfwrite
+chmod ugo+x root/usr/libexec/cups/backend/pswrite
+
+chmod ugo+x scripts/postinstall
+
+# Build the package.
 pkgbuild --identifier com.me.ben.printqueues --root root --scripts scripts BensPrintQueues.pkg
+
+# Cleanup.
+rm root/Library/Printers/PPDs/Bens/*
+rm root/usr/libexec/cups/backend/*
+
+rmdir root/Library/Printers/PPDs/Bens
+rmdir root/Library/Printers/PPDs
+rmdir root/Library/Printers
+rmdir root/Library
+rmdir root/usr/libexec/cups/backend
+rmdir root/usr/libexec/cups
+rmdir root/usr/libexec
+rmdir root/usr
+rmdir root
+
+rm scripts/postinstall
+
+rmdir scripts
